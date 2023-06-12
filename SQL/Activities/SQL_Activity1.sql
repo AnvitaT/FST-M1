f6881d0f7363f840ea8dd2f35b9aaa7538663867
AnvitaT <132567989+AnvitaT@users.noreply.github.com>
2023-06-12 12:50:11 +0530

    Add files via upload

---
+++ SQL/Activities/SQL_Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: SQL-Activity1
+REM   Creation of Salesman table
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
